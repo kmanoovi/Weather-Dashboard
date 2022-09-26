99919fb2b7ad3300b4a8e14bb9ccd5f5c09bb0ec
Katie Mandovi <ktmandovi@MacBook-Pro.local>
2022-09-25 21:36:38 -0400

    added to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Weather-Dashboard
+
+I used Bootstrap to help curate a simple layout and design. I edited some of the html
+and added to the css as well.
+
+The Weather API I used was given for this project and I utilized it in the
+javascript file.
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 var weatherAPIKey = "12996bf19be251eb85850d90fc1b3859";
 var city;
 
-var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
 
-fetch(url)
+
+function getInfo() {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
+  
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+        }
+      });
+  }
+  searchButton.addEventListener('click', getInfo);
